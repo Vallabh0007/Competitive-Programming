8d4b081499734ed2003150f1b727ea7fdd9c87fb
Vallabh0007 <67462145+Vallabh0007@users.noreply.github.com>
2021-03-19 09:59:20 +0530

    LeetCode first problem commit

---
+++ LeetCode/Search_Insert.py
@@ -0,0 +1,17 @@
+#problem-https://leetcode.com/problems/search-insert-position/
+#author-Vallabh
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = round(left + (right - left) / 2)
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid -1
+            else:
+                left = mid + 1
+                
+        return left
